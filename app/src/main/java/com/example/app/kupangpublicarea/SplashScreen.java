f0a055f0529322ee7b40c0dd077edc710b916652
Arthur Pello <arthurnumen12@gmail.com>
2016-03-23 11:46:46 +0700

    first commit

---
+++ app/src/main/java/com/example/app/kupangpublicarea/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.app.kupangpublicarea;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by arthurnumen on 3/20/16.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MapsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
