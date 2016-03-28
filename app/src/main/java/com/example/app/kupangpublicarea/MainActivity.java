01b0dabc405649531fd291d4123b96f9ee8b6297
Arthur Pello <arthurnumen12@gmail.com>
2016-03-28 19:36:02 +0700

    update button action

--- app/src/main/java/com/example/app/kupangpublicarea/MainActivity.java
+++ app/src/main/java/com/example/app/kupangpublicarea/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.app.kupangpublicarea;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,37 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        Button petaSPBU = (Button)findViewById(R.id.button);
+        Button daftarSPBU = (Button)findViewById(R.id.button2);
+        Button infoAplikasi = (Button)findViewById(R.id.button3);
+        Button exit = (Button)findViewById(R.id.button4);
+
+        petaSPBU.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent i = new Intent(getApplicationContext(),MapsActivity.class);
+
+                startActivity(i);
+            }
+        });
+
+        daftarSPBU.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+            }
+        });
+
+        infoAplikasi.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                
+            }
+        });
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                moveTaskToBack(true);
+            }
+        });
+
     }
 }
